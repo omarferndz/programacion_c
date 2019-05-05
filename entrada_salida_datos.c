3d47958d16ca20c1ada7178abfd3fc55d5e4c8bc
omarferndz <omarferndz@gmail.com>
2019-05-05 18:59:45 -0400

    entrada y salida de datos

---
+++ entrada_salida_datos.c
@@ -0,0 +1,16 @@
+//Entradas y Salidas de Datos
+
+#include<stdio.h>
+
+int main(){
+    int a; 
+    float b;
+    char c;
+    
+    printf("Digite el valor de la variable c: ");
+    scanf("%c",&c);
+
+    printf("El valor es: %c\n",c);
+    
+    return 0;
+}
